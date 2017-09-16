0731b3a7c65893d27632a82a1be9fc85ac41da1a
ajlopez <webmaster@ajlopez.com>
2017-09-16 18:16:37 -0300

    Execute transfer command

--- tests/lib/commands.js
+++ tests/lib/commands.js
@@ -1,11 +1,21 @@
 
-function TransferCommand() {
-	
+function TransferCommand(cmd, world) {
+	this.execute = function (txdata, cb) {
+		var data = {
+			from: txdata.from,
+			to: txdata.to,
+			value: txdata.value,
+			gasPrice: 1,
+			gas: 21000
+		};
+		
+		world.server().sendTransfer(data, cb);
+	}
 }
 
-function createCommand(cmd) {
+function createCommand(cmd, world) {
 	if (cmd.command === 'transfer')
-		return new TransferCommand();
+		return new TransferCommand(cmd, world);
 }
 
 module.exports = {
--- tests/test/commands.js
+++ tests/test/commands.js
@@ -1,10 +1,47 @@
 
 var commands = require('../lib/commands');
+var worlds = require('../lib/worlds');
 
 exports['create transfer command'] = function (test) {
 	var cmd = commands.command({ command: 'transfer' });
 	
 	test.ok(cmd);
+};
+
+exports['execute transfer command'] = function (test) {
+	test.async();
+	
+	var world = worlds.world();
+	
+	var server = {
+		sendTransfer: function (data, cb) {
+			test.ok(data);
+			test.equal(data.from, txdata.from);
+			test.equal(data.to, txdata.to);
+			test.equal(data.value, 100000);
+			test.equal(data.gasPrice, 1);
+			test.equal(data.gas, 21000);
+			
+			cb(null, null);
+		}
+	}
+	
+	world.server(server);
+	
+	var cmd = commands.command({ command: 'transfer' }, world);
+	
+	test.ok(cmd);
+	
+	var txdata = {
+		from: 'from',
+		to: 'to',
+		value: 100000     
+	};
+	
+	cmd.execute(txdata, function (err, data) {
+		test.equal(err, null);
+		test.done();
+	});
 };
 
 
